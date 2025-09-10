017147b9baac991a61fd910dfab111ecdd23ef52
vitoria <vitoriakotowski@gmail.com>
2025-09-09 18:52:26 -0700

    add /api/v1/status endpoint and remove dummy test

--- models/calulator.js
+++
@@ -1,8 +0,0 @@
-function addUp(number1, number2) {
-  if (typeof number1 !== "number") {
-    return "Error";
-  }
-  return number1 + number2;
-}
-
-exports.addUp = addUp;
---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,5 @@
+function status(request, response) {
+  response.status(200).json({ key: "aaaaaaa" });
+}
+
+export default status;
