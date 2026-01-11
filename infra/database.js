867839b800a9f0cd9fa40a22c45da900ca7ad5f7
vitoria <vitoriakotowski@gmail.com>
2026-01-10 18:57:38 -0800

    remove console.log

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,6 @@ async function query(queryObjects) {
     password: process.env.POSTGRES_PASSWORD,
     ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  console.log("Postgres Credentials:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
 
   try {
     await client.connect();
